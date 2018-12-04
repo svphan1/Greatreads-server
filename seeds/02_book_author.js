9d690c3e4b9bb80d78f9295615a61a00a98c31ca
svphan29 <36240410+svphan29@users.noreply.github.com>
2018-12-04 14:10:44 -0700

    complete join table seed

--- seeds/02_book_author.js
+++ seeds/02_book_author.js
@@ -1,13 +1,48 @@
 
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('book_author').del()
     .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('book_author').insert([
+        {
+          id: 1, 
+          author_id: 1,
+          book_id: 1
+        },
+        {
+          id: 2, 
+          author_id: 2,
+          book_id: 1
+        },
+        {
+          id: 3, 
+          author_id: 3,
+          book_id: 1
+        },
+        {
+          id: 4, 
+          author_id: 4,
+          book_id: 2
+        },
+        {
+          id: 5, 
+          author_id: 5,
+          book_id: 3
+        },
+        {
+          id: 6, 
+          author_id: 6,
+          book_id: 4
+        },
+        {
+          id: 7, 
+          author_id: 6,
+          book_id: 5
+        },
+        {
+          id: 8, 
+          author_id: 6,
+          book_id: 6
+        },
       ]);
     });
 };
--- seeds/03_authors.js
+++ seeds/03_authors.js
@@ -1,10 +1,8 @@
 
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('authors').del()
     .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
+      return knex('authors').insert([
         {
           id: 1, 
           firstName: 'Alex',
@@ -14,20 +12,34 @@ exports.seed = function(knex, Promise) {
         },
         {
           id: 2, 
+          firstName: 'Anna',
+          lastName: 'Ravenscroft',
+          biography: `Anna Martelli Ravenscroft is an experienced speaker and trainer, with diverse background developing curricula for church, regional transit, disaster preparedness; developing web applications for therapy, learning, fitness; writing technical books, articles and presentations; active member of Open Source community; skilled at translating between IT professionals and end users.`,
+          portraitURL: 'https://images-na.ssl-images-amazon.com/images/I/A1ufyxpz++L._UX250_.jpg'
+        },
+        {
+          id: 3, 
+          firstName: 'Steve',
+          lastName: 'Holden',
+          biography: `Steve Holden Is a consultant, advising clients on system and network architectures and the design and implementation of programmed web systems. He also teaches classes on TCP/IP, network security, database and programming topics, and is the author of \"Python Web Programming\", the O'Reilly School of Technology's \"Certificate series in Python\" and O'Reilly Media's \"Intermediate Python\" video series.`,
+          portraitURL: 'http://cdn.oreillystatic.com/images/people/154/steve_holden.jpg'
+        },
+        {
+          id: 4, 
           firstName: 'Allen B.',
           lastName: 'Downey',
           biography: `Allen Downey is a Professor of Computer Science at Olin College of Engineering. He has taught at Wellesley College, Colby College and U.C. Berkeley. He has a Ph.D. in Computer Science from U.C. Berkeley and Master's and Bachelor's degrees from MIT.`,
           portraitURL: 'http://cdn.oreillystatic.com/images/people/154/allen_downey.jpg'
         },
         {
-          id: 3, 
+          id: 5, 
           firstName: 'Bonnie',
           lastName: 'Eisenman',
           biography: 'Bonnie Eisenman is a software engineer at Codecademy, with previous experience at Fog Creek Software and Google. She has spoken at several conferences on topics ranging from ReactJS to musical programming and Arduinos. In her spare time, she enjoys building electronic musical instruments, tinkering with hardware projects, and laser-cutting chocolate. Find her on Twitter as @brindelle.',
           portraitURL: 'http://cdn.oreillystatic.com/images/people/154/bonnie_eisenman.jpg'
         },
         {
-          id: 3, 
+          id: 6, 
           firstName: 'Kyle',
           lastName: 'Simpson',
           biography: `Kyle Simpson is an Open Web Evangelist who's passionate about all things JavaScript. He's an author, workshop trainer, tech speaker, and OSS contributor/leader.`,

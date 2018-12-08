f1f967f398514a6420b45189017cd5927bbd3b96
svphan29 <36240410+svphan29@users.noreply.github.com>
2018-12-07 21:23:44 -0700

    updated database

--- app.js
+++ app.js
@@ -1,34 +1,35 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const booksRoute = require('./routes/books');
-const authorsRoute = require('./routes/authors');
-const bookAuthorRoute = require('./routes/book_author');
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const booksRoute = require("./routes/books");
+const authorsRoute = require("./routes/authors");
+const bookAuthorRoute = require("./routes/book_author");
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors()); 
 
-app.use('/books', booksRoute);
-app.use('/authors', authorsRoute);
-app.use('/all', bookAuthorRoute);
+app.use("/books", booksRoute);
+app.use("/authors", authorsRoute);
+app.use("/all", bookAuthorRoute);
 
-app.get('/', (req, res) => {
-  res.send('Home')
-});
+app.get("/", (req, res) => res.json({
+  "Books": "http://localhost:3000/books",
+  "Authors": "http://localhost:3000/authors"
+}));
 
 app.use(notFound);
 app.use(errorHandler);
 
 function notFound(err, req, res, next) {
-  res.status(404).send({error: 'Not found!', status: 404, url: req.originalUrl})
+  res.status(404).send({error: "Not found!", status: 404, url: req.originalUrl})
 };
 
 function errorHandler(err, req, res, next) {
-  console.error('Error', err)
-  const stack =  process.env.NODE_ENV !== 'production' ? err.stack : undefined
+  console.error("Error", err)
+  const stack =  process.env.NODE_ENV !== "production" ? err.stack : undefined
   res.status(500).send({error: err.message, stack, url: req.originalUrl})
 };
 
--- seeds/01_books.js
+++ seeds/01_books.js
@@ -1,54 +1,70 @@
 exports.seed = function(knex, Promise) {
-  return knex.raw('DELETE FROM books; ALTER SEQUENCE books_id_seq RESTART WITH 7')
+  return knex.raw("DELETE FROM books; ALTER SEQUENCE books_id_seq RESTART WITH 7")
     .then(function () {
-      return knex('books').insert([
+      return knex("books").insert([
         {
           id: 1, 
-          title: 'Python In A Nutshell',
-          genre: 'Python',
-          description: 'This book offers Python programmers one place to look when they need help remembering or deciphering the syntax of this open source language and its many powerful but scantily documented modules. This comprehensive reference guide makes it easy to look up the most frequently needed information--not just about the Python language itself, but also the most frequently used parts of the standard library and the most important third-party extensions.',
-          coverUrl: 'https://covers.oreillystatic.com/images/0636920012610/lrg.jpg',
-          authors: 'Alex Martelli, Anna Ravenscroft, Steve Holden'
+          title: "To Kill a Mockingbird",
+          genre: "Fiction",
+          description: "The unforgettable novel of a childhood in a sleepy Southern town and the crisis of conscience that rocked it, To Kill A Mockingbird became both an instant bestseller and a critical success when it was first published in 1960. It went on to win the Pulitzer Prize in 1961 and was later made into an Academy Award-winning film, also a classic.",
+          coverUrl: "https://images.gr-assets.com/books/1361975680l/2657.jpg",
+          authors: "Harper Lee"
         },
         {
-          id: 2,
-          title: 'Think Python',
-          genre: 'Python',
-          description: 'If you want to learn how to program, working with Python is an excellent way to start. This hands-on guide takes you through the language a step at a time, beginning with basic programming concepts before moving on to functions, recursion, data structures, and object-oriented design. This second edition and its supporting code have been updated for Python 3.',
-          coverUrl: 'https://covers.oreillystatic.com/images/0636920025696/lrg.jpg',
-          authors: 'Allen B. Downey'
+          id: 2, 
+          title: "The Hitchhiker's Guide to the Galaxy",
+          genre: "Fiction",
+          description: "This book offers Python programmers one place to look when they need help remembering or deciphering the syntax of this open source language and its many powerful but scantily documented modules. This comprehensive reference guide makes it easy to look up the most frequently needed information--not just about the Python language itself, but also the most frequently used parts of the standard library and the most important third-party extensions.",
+          coverUrl: "https://images.gr-assets.com/books/1531891848l/11.jpg",
+          authors: "Douglas Adams"
         },
         {
           id: 3, 
-          title: 'Learning React Native',
-          genre: 'JavaScript',
-          description: 'Get a practical introduction to React Native, the JavaScript framework for writing and deploying fully featured mobile apps that look and feel native. With this hands-on guide, you’ll learn how to build applications that target iOS, Android, and other mobile platforms instead of browsers. You’ll also discover how to access platform features such as the camera, user location, and local storage.',
-          coverUrl: 'https://covers.oreillystatic.com/images/0636920085270/lrg.jpg',
-          authors: 'Bonnie Eisenman'
+          title: "Python In A Nutshell",
+          genre: "Python",
+          description: "This book offers Python programmers one place to look when they need help remembering or deciphering the syntax of this open source language and its many powerful but scantily documented modules. This comprehensive reference guide makes it easy to look up the most frequently needed information--not just about the Python language itself, but also the most frequently used parts of the standard library and the most important third-party extensions.",
+          coverUrl: "https://covers.oreillystatic.com/images/0636920012610/lrg.jpg",
+          authors: "Alex Martelli, Anna Ravenscroft, Steve Holden"
         },
         {
-          id: 4, 
-          title: `You Don't Know JS: ES6 & Beyond`,
-          genre: 'JavaScript',
-          description: 'No matter how much experience you have with JavaScript, odds are you don’t fully understand the language. As part of the \"You Don’t Know JS\" series, this compact guide focuses on new features available in ECMAScript 6 (ES6), the latest version of the standard upon which JavaScript is built.',
-          coverUrl: 'https://covers.oreillystatic.com/images/0636920033769/lrg.jpg',
-          authors: 'Kyle Simpson'
+          id: 4,
+          title: "Think Python",
+          genre: "Python",
+          description: "If you want to learn how to program, working with Python is an excellent way to start. This hands-on guide takes you through the language a step at a time, beginning with basic programming concepts before moving on to functions, recursion, data structures, and object-oriented design. This second edition and its supporting code have been updated for Python 3.",
+          coverUrl: "https://covers.oreillystatic.com/images/0636920025696/lrg.jpg",
+          authors: "Allen B. Downey"
         },
         {
           id: 5, 
-          title: `You Don't Know JS: Scope & Closures`,
-          genre: 'JavaScript',
-          description: 'No matter how much experience you have with JavaScript, odds are you don’t fully understand the language. This concise yet in-depth guide takes you inside scope and closures, two core concepts you need to know to become a more efficient and effective JavaScript programmer. You’ll learn how and why they work, and how an understanding of closures can be a powerful part of your development skillset.',
-          coverUrl: 'https://covers.oreillystatic.com/images/0636920026327/lrg.jpg',
-          authors: 'Kyle Simpson'
+          title: "Learning React Native",
+          genre: "JavaScript",
+          description: "Get a practical introduction to React Native, the JavaScript framework for writing and deploying fully featured mobile apps that look and feel native. With this hands-on guide, you’ll learn how to build applications that target iOS, Android, and other mobile platforms instead of browsers. You’ll also discover how to access platform features such as the camera, user location, and local storage.",
+          coverUrl: "https://covers.oreillystatic.com/images/0636920085270/lrg.jpg",
+          authors: "Bonnie Eisenman"
         },
         {
           id: 6, 
-          title: `You Don't Know JS: Async & Performance`,
-          genre: 'JavaScript',
-          description: 'No matter how much experience you have with JavaScript, odds are you don’t fully understand the language. As part of the \"You Don’t Know JS\" series, this concise yet in-depth guide focuses on new asynchronous features and performance techniques—including Promises, generators, and Web Workers—that let you create sophisticated single-page web applications and escape callback hell in the process.',
-          coverUrl: 'https://covers.oreillystatic.com/images/0636920033752/lrg.jpg',
-          authors: 'Kyle Simpson'
+          title: `You Don"t Know JS: ES6 & Beyond`,
+          genre: "JavaScript",
+          description: "No matter how much experience you have with JavaScript, odds are you don’t fully understand the language. As part of the \"You Don’t Know JS\" series, this compact guide focuses on new features available in ECMAScript 6 (ES6), the latest version of the standard upon which JavaScript is built.",
+          coverUrl: "https://covers.oreillystatic.com/images/0636920033769/lrg.jpg",
+          authors: "Kyle Simpson"
+        },
+        {
+          id: 7, 
+          title: `You Don"t Know JS: Scope & Closures`,
+          genre: "JavaScript",
+          description: "No matter how much experience you have with JavaScript, odds are you don’t fully understand the language. This concise yet in-depth guide takes you inside scope and closures, two core concepts you need to know to become a more efficient and effective JavaScript programmer. You’ll learn how and why they work, and how an understanding of closures can be a powerful part of your development skillset.",
+          coverUrl: "https://covers.oreillystatic.com/images/0636920026327/lrg.jpg",
+          authors: "Kyle Simpson"
+        },
+        {
+          id: 8, 
+          title: `You Don"t Know JS: Async & Performance`,
+          genre: "JavaScript",
+          description: "No matter how much experience you have with JavaScript, odds are you don’t fully understand the language. As part of the \"You Don’t Know JS\" series, this concise yet in-depth guide focuses on new asynchronous features and performance techniques—including Promises, generators, and Web Workers—that let you create sophisticated single-page web applications and escape callback hell in the process.",
+          coverUrl: "https://covers.oreillystatic.com/images/0636920033752/lrg.jpg",
+          authors: "Kyle Simpson"
         },
       ]);
     });
